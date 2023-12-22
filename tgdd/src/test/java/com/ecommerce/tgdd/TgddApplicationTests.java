966c9aa724bc269c9ec02ccfdb768f1e8f89f579
Thắng Em Nguyễn <thangemn@gmail.com>
2023-12-22 15:31:10 +0700

    create base + git

---
+++ tgdd/src/test/java/com/ecommerce/tgdd/TgddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.tgdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
